eb9507d06cc7452a33100f370c9a7d71e1f08d4f
anmolsingh7275 <anmolprogramming@gmail.com>
2025-09-06 23:28:27 +0530

    Add Toast Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./index.css"; // ✅ Must import Tailwind CSS
+import { ToastProvider } from "./Components/Toast"; 
 
 createRoot(document.getElementById('root')).render(
  
   <BrowserRouter>
   
-    <App />
-
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </BrowserRouter>
 )
